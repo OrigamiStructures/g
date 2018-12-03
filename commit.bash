b8e449c2208d17f9d336394f85f3a4ee8975c7e5
World Wide Web Server <_www@dlocal.local>
2018-12-02 20:22:56 -0800

    adding-foundation-to-our-dev-repos

--- commit.bash
+++ commit.bash
@@ -1,3 +1,3 @@
 cd /Library/WebServer/Documents/g 2>&1
 git add -A 2>&1
-git commit -m 'callables-and-callbacks-a-php-and-cakephp3-x-case-study' 2>&1
+git commit -m 'adding-foundation-to-our-dev-repos' 2>&1
