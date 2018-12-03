b84713de76d888768f7731206ce6401d7bc80548
World Wide Web Server <_www@dlocal.local>
2018-12-02 20:38:57 -0800

    apache-server-settings-to-streamline-cakephp-installations-in-mac-os-x

--- commit.bash
+++ commit.bash
@@ -1,3 +1,3 @@
 cd /Library/WebServer/Documents/g 2>&1
 git add -A 2>&1
-git commit -m 'markdown-plugin-documentation' 2>&1
+git commit -m 'apache-server-settings-to-streamline-cakephp-installations-in-mac-os-x' 2>&1
